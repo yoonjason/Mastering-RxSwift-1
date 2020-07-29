b162f80f964197be5aaee3df1f5c8081713e56a9
yoon <luffy1021@naver.com>
2020-07-29 18:53:58 +0900

    * kxCoding을 보며 진행하는 RxSwift Study

--- main/Hello/Hello.playground/Contents.swift
+++ main/Hello/Hello.playground/Contents.swift
@@ -23,6 +23,22 @@
 import UIKit
 import RxSwift
 
+let disposeBag = DisposeBag()
+
+Observable.just("Hello, RxSwift")
+    .subscribe{print($0)}
+    .disposed(by: disposeBag)
+
+let a = BehaviorSubject(value: 1)
+let b = BehaviorSubject(value: 2)
+
+Observable.combineLatest(a, b){$0 + $1}
+    .subscribe(onNext : {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+a.onNext(12)
+
 
 
 
